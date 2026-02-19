9918a4ea0bec431d2bc5a3a00db32a9eed489b65
DaechwitaWhyTho <2305169@ugrad.cse.buet.ac.bd>
2026-02-19 14:10:28 +0600

    Fixed token generation for buyer,seller; checked new routes

--- ScreenShop/backend/routes/authRoutes.js
+++ ScreenShop/backend/routes/authRoutes.js
@@ -1,3 +1,4 @@
+import { sql } from "../config/db.js";
 import express from "express";
 import { register, login } from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
